95239cd5b1c79ab5be81460322263c5695267a06
Nahom <zerihunnahom@gmail.com>
2023-01-30 22:23:08 +0300

    Refactor html ,css and js file to finish the menu

--- index.js
+++ index.js
@@ -1,15 +1,25 @@
 const mobileMenu = document.querySelector('.mobile_menu');
-const openMenu = document.querySelector('.portfolio-nav--menu');
-const closeMenu = document.querySelector('.mobile_menu button');
+const nav = document.querySelector('nav');
+const sections = document.querySelectorAll('section');
+const menuButton = document.querySelector('.portfolio-nav--menu');
+const closeButton = document.querySelector('.mobile_menu button');
 const menuOptions = document.querySelectorAll('.mobile_menu-content a');
 function show() {
   mobileMenu.style.display = 'block';
+  nav.style.filter = 'blur(5px)';
+  sections.forEach((section) => {
+    section.style.filter = 'blur(5px)';
+  });
 }
 function close() {
   mobileMenu.style.display = 'none';
+  nav.style.filter = 'blur(0)';
+  sections.forEach((section) => {
+    section.style.filter = 'blur(0)';
+  });
 }
-openMenu.addEventListener('click', show);
-closeMenu.addEventListener('click', close);
+menuButton.addEventListener('click', show);
+closeButton.addEventListener('click', close);
 menuOptions.forEach((option) => {
   option.addEventListener('click', close);
 });
